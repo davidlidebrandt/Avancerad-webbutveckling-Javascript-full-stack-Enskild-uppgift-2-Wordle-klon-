d271dbd92f5feb9c3200b4195a7fd3b78f1ff67f
David Lidebrandt <david.lidebrandt@gmail.com>
2023-03-27 03:17:57 +0200

    Add header with nav

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+const gameLink = document.querySelector(".game-link");
+const highScoresLink = document.querySelector(".high-scores-link");
+const infoLink = document.querySelector(".info-link");
+let allMainNavLinks = document.querySelectorAll(".main-nav-link")
+const currentPath = window.location.pathname;
+
+allMainNavLinks = Array.from(allMainNavLinks);
+allMainNavLinks.forEach((item)=> {
+    item.classList.remove("active");
+});
+
+if(currentPath === "/") {
+    gameLink.classList.add("active");
+}
+if(currentPath === "/high-scores") {
+    highScoresLink.classList.add("active");
+}
+
+if(currentPath === "/information") {
+    infoLink.classList.add("active");
+}
