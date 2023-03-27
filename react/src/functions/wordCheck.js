db815e81a23d8f483ed1d290f03611ae2313cf08
David Lidebrandt <david.lidebrandt@gmail.com>
2023-03-27 23:55:55 +0200

    Add word filtering functions

---
+++ react/src/functions/wordCheck.js
@@ -0,0 +1,41 @@
+export default function wordCheck(inputWord, correctWord){
+    const checkedLetters = Array(inputWord.length).fill({});
+    correctWord = correctWord.split("");
+    inputWord = inputWord.split("");
+    
+    // Find all letters at correct positions and mark them as 'found'
+    // to ensure they won't be counted more than once.
+    inputWord.forEach((letter, index) => {
+        const currentLetter = letter;
+        if(correctWord[index] === inputWord[index]) {
+            const letterAndValue = {};
+            letterAndValue[currentLetter.toUpperCase()] = "correct";
+            checkedLetters[index] = letterAndValue;
+            inputWord[index] = null;
+            correctWord[index] = null;
+        }
+    });
+
+    // Find misplaced letters and mark them as 'found'
+    // to ensure they won't be counted more than once.
+    // Mark all other letters as incorrect.
+    inputWord.forEach((letter, index)=> {
+        const currentLetter = letter;
+        if(letter != null && correctWord.includes(letter)) {
+            const letterAndValue = {};
+            letterAndValue[currentLetter.toUpperCase()] = "misplaced";
+            checkedLetters[index] = letterAndValue;
+            inputWord[index] = null;
+            correctWord[index] = null;
+        }
+        else if(letter != null) {
+            const letterAndValue = {};
+            letterAndValue[currentLetter.toUpperCase()] = "incorrect";
+            checkedLetters[index] = letterAndValue;
+            inputWord[index] = null;
+            correctWord[index] = null;
+        }
+    });
+    
+    return checkedLetters;
+}
