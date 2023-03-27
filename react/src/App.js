b22e43120ddef98570a1e0d7b293a1d8e6353865
David Lidebrandt <david.lidebrandt@gmail.com>
2023-03-27 19:53:09 +0200

    Add input fields

--- react/src/App.js
+++ react/src/App.js
@@ -1,6 +1,18 @@
+import logo from "./logo.png";
+
 import React from "react";
 const App = () => {
-  return <h1>Hello duuuuu</h1>;
+  return (
+  <div className="container">
+     <h2><img src={logo}></img></h2>
+     <form>
+     <label for="letters">Choose the number of letters (between 4 and 6)</label>
+     <input required type="number" id="letters" name="letters" min="4" max="6"></input>
+     <label for="duplicateLetters"> Can contain duplicate letters?</label>
+     <input type="checkbox" id="duplicateLetters" name="duplicateLetters" value="true"></input>
+     <button type="submit">Start Game</button>
+     </form>
+  </div>)
 };
 
 export default App;
