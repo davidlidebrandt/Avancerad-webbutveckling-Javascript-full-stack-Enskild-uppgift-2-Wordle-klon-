479bbb21aff9273ce45efc63504684e25c9b97c5
David Lidebrandt <david.lidebrandt@gmail.com>
2023-03-28 17:27:35 +0200

    Create letter boxes

--- react/src/components/Game.js
+++ react/src/components/Game.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import "../scss/Game.scss";
+
+const letters = [
+    {letter: "w", correct: true, misplaced: false},
+    {letter: "k", correct: false, misplaced: true},
+    {letter: "s", correct: false, misplaced: false},
+    {letter: "i", correct: false, misplaced: false},
+]
 
 export default function Game() {
+  const {correctWord, setCorrectWord} = useState("wind");
+
+  let currentLetters = letters.map((l, i)=> {
+    if(l.correct) {
+        return <div key={i} className='letter-box correct-letter'>{l.letter}</div>
+    }
+    else if(l.misplaced) {
+        return <div key={i} className='letter-box misplaced-letter'>{l.letter}</div>
+    }
+    else {
+        return <div key={i} className='letter-box'>{l.letter}</div>
+    }
+  });
+  console.log(currentLetters)
   return (
-    <div>Game</div>
+    <div className='game-container'>
+        {currentLetters}
+    </div>
   )
 }
--- react/src/index.js
+++ react/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as ReactDOMClient from 'react-dom/client';
 import App from "./App";
-import "./App.scss";
+import "./scss/App.scss";
 
 const el = document.getElementById("app");
 let root = ReactDOMClient.createRoot(el);
