5a0a8ca0bb51dcc6678142d699631fc63f56f688
David Lidebrandt <david.lidebrandt@gmail.com>
2023-04-03 03:45:59 +0200

    Add enpoint for retriving unique word

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import nunjucks from "nunjucks";
 import axios from "axios";
+import fs from "fs";
+import filterGameOptions from "./server/js/filterGameOptions.js";
 
 const app = express();
 
@@ -13,6 +15,8 @@ app.set('view engine', 'html');
 
 app.use(express.static("./public"));
 
+app.use(express.json());
+
 app.get("/", (req,res)=> {
     res.render("index.html");
 });
@@ -23,6 +27,27 @@ app.get("/information", (req,res)=> {
 
 app.get("/high-scores", (req,res)=> {
     res.render("high-scores.html");
+});
+
+app.get("/api/word", async (req,res)=> {
+    let numberOfLetters = req.body.numberOfLetters;
+    let duplicateLetters = req.body.duplicateLetters;
+
+    if(isNaN(numberOfLetters) || numberOfLetters < 4 || numberOfLetters > 6 || typeof duplicateLetters !== "boolean") {
+        res.status(422).json({error: "Invalid data"});
+    }
+       
+    try {
+        let words = fs.readFileSync("./server/words.json", "utf-8")
+        words = JSON.parse(words);
+        words = Object.keys(words);
+        const word = filterGameOptions(words, 5, true);
+        res.status(201).json({data: word});
+        
+    } catch (error) {
+        res.status(500).json({error: "Error fetching the word, please try again"});
+    }
+   
 });
 
 export default app;
